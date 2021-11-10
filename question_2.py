9787329ab47aff3473c5af722c78c6535b5771d2
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-11-10 07:46:51 +0600

    2 solved partially

--- question_2.py
+++ question_2.py
@@ -1,6 +1,17 @@
 import utility as util
 
-number = 13.5001
+number = 3.51
 
-print(util.round_to_closest_int(number))
+end = util.round_to_closest_int(number)
 
+arr = list(range(1, end + 1))
+arr = arr[::-1]
+# print(arr)
+
+i = 0
+
+while i != len(arr):
+    for j in range(i, len(arr)):
+        print(arr[j], end= ' ')
+    print()
+    i = i + 1
--- utility.py
+++ utility.py
@@ -1,6 +1,6 @@
 def round_to_closest_int(number):
     rounded_dec_2 = round(number, 2)
-    print(rounded_dec_2)
+    # print(rounded_dec_2)
     decimals = (rounded_dec_2 * 100) % 100
 
     if decimals <= 50:
