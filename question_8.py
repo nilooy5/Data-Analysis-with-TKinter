0c685ad59954812cdb02e78da7137ae63f61f654
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-11-10 20:02:00 +0600

    question_8.py properly completed

--- question_8.py
+++ question_8.py
@@ -1,37 +1,39 @@
+import numpy
 from sklearn import datasets, neighbors, metrics, svm
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 dataset = datasets.load_iris()
-print(dataset)
+# print(dataset)
 
 X = dataset.data
-print('Array of data samples:')
-print(X)
-print()
+# print('Array of data samples:')
+# print(X)
+# print()
 n_samples, n_features = X.shape
 
-print('Number of data samples: ', n_samples)
-print('Dimensionality (Number of features): ', n_features)
-print()
+# print('Number of data samples: ', n_samples)
+# print('Dimensionality (Number of features): ', n_features)
+# print()
 y = dataset.target
-print('True class index of data samples:')
-print(y)
-print()
+# print('True class index of data samples:')
+# print(y)
+# print()
 class_names = dataset.target_names
-print('Array of class names:', class_names)
-print('Number of classes:', len(class_names))
-print()
+# print('Array of class names:', class_names)
+# print('Number of classes:', len(class_names))
+# print()
 
 # Split dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-print(X_train)
-print()
-print(X_test)
-print()
-print(y_train)
-print()
-print(y_test)
-print()
+# print(X_train)
+# print()
+# print(X_test)
+# print()
+# print(y_train)
+# print()
+# print(y_test)
+# print()
 
 # Load classifier containing classification technique and model
 classifier = neighbors.KNeighborsClassifier(n_neighbors=3)
@@ -43,22 +45,13 @@ y_pred = classifier.predict(X_test)
 
 
 def get_confusion_matrix(true_list, predicted_list, class_labels):
-    c_matrix = metrics.confusion_matrix(true_list, predicted_list)
-    print(f"%35s" % " ", end='')
-    for i in class_labels:
-        print(f"%35s" % i, end='')
-    print()
-    p = 0
-    for i in c_matrix:
-        print(f"%35s" % class_labels[p], end='')
-        for j in i:
-            print(f"%35s" % j, end='')
-        print()
-        p = p + 1
-
+    c_matrix = pd.crosstab(true_list, predicted_list)
+    c_matrix = c_matrix.set_axis(class_labels, axis=0, inplace=False)
+    c_matrix = c_matrix.set_axis(class_labels, axis=1, inplace=False)
     return c_matrix
 
 
-label_names = ['Setosa', 'Versicolour', 'Virginica']
+label_names = numpy.array(['Setosa', 'Versicolour', 'Virginica'])
 confusion_matrix = get_confusion_matrix(y_pred, y_test, label_names)
+print(confusion_matrix)
 
