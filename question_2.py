5ff3a5ce479bcd69af8db32a4264a9dfecb0e5ab
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-11-10 15:35:07 +0600

    question_2.py put in method

--- question_2.py
+++ question_2.py
@@ -1,27 +1,32 @@
 import utility as util
 
 
-while True:
-    input_num = input("Enter a number: ")
+def my_printing():
+    while True:
+        input_num = input("Enter a number: ")
 
-    try:
-        number = float(input_num)
-    except ValueError:
-        print("only numbers are accepted")
-        continue
+        try:
+            number = float(input_num)
+        except ValueError:
+            print("only numbers are accepted")
+            continue
 
-    end = util.round_to_closest_int(number)
+        end = util.round_to_closest_int(number)
 
-    arr = list(range(1, end + 1))
-    arr = arr[::-1]
+        arr = list(range(1, end + 1))
+        arr = arr[::-1]
 
-    i = 0
+        i = 0
 
-    while i != len(arr):
-        for j in range(i, len(arr)):
-            if number < 0:
-                print(arr[j] * -1, end=" ")
-            else:
-                print(arr[j], end=' ')
-        print()
-        i = i + 1
+        while i != len(arr):
+            for j in range(i, len(arr)):
+                if number < 0:
+                    print(arr[j] * -1, end=" ")
+                else:
+                    print(arr[j], end=' ')
+            print()
+            i = i + 1
+
+
+my_printing()
+
