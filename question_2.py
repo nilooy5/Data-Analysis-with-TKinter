e571e7b1efe403eb6d3b21e759406ad597cc45fb
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-11-10 07:57:23 +0600

    2 solved for positive numbers

--- question_2.py
+++ question_2.py
@@ -1,17 +1,24 @@
 import utility as util
 
-number = 3.51
 
-end = util.round_to_closest_int(number)
+while True:
+    input_num = input("Enter a number: ")
 
-arr = list(range(1, end + 1))
-arr = arr[::-1]
-# print(arr)
+    try:
+        number = float(input_num)
+    except ValueError:
+        print("only numbers are accepted")
+        continue
 
-i = 0
+    end = util.round_to_closest_int(number)
 
-while i != len(arr):
-    for j in range(i, len(arr)):
-        print(arr[j], end= ' ')
-    print()
-    i = i + 1
+    arr = list(range(1, end + 1))
+    arr = arr[::-1]
+
+    i = 0
+
+    while i != len(arr):
+        for j in range(i, len(arr)):
+            print(arr[j], end=' ')
+        print()
+        i = i + 1
