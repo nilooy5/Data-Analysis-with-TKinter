3b3e71989250b22f451e86a4e55239072a1da8f8
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-11-10 09:54:09 +0600

    q3 completed

--- question_3.py
+++ question_3.py
@@ -1,44 +1,48 @@
 import numpy as np
 import io_data_module as io
 
-dataset = io.read_multi_dim_data("Files_Assignment3/iris.data")
 
-labels_dict = {}
-for item in dataset:
-    item = list(item)
-    if item[-1] not in labels_dict:
-        labels_dict[item[-1]] = [item[:-1]]
-    else:
-        labels_dict[item[-1]].append(list(item[:-1]))
+def load_iris_dataset(filename):
+    dataset = io.read_multi_dim_data("Files_Assignment3/" + str(filename))
 
-class_labels = list(labels_dict.keys())
+    labels_dict = {}
+    for item in dataset:
+        item = list(item)
+        if item[-1] not in labels_dict:
+            labels_dict[item[-1]] = [item[:-1]]
+        else:
+            labels_dict[item[-1]].append(list(item[:-1]))
 
-i = 0
+    class_labels = list(labels_dict.keys())
 
-X = []
-y = []
+    i = 0
 
-for item in dataset:
-    item = list(item)
-    if i % 2 == 1:
-        print(item[0], item[1], item[2], item[3], item[4])
-        temp = []
-        for j in item[:-1]:
-            temp.append(float(j))
-        X.append(temp)
-        temp = []
-        if item[-1] == 'Iris-setosa':
-            y.append(0)
-        elif item[-1] == 'Iris-versicolor':
-            y.append(1)
-        elif item[-1] == 'Iris-virginica':
-            y.append(2)
+    X = []
+    y = []
 
-    i = i + 1
+    for item in dataset:
+        item = list(item)
+        if i % 2 == 1:
+            temp = []
+            for j in item[:-1]:
+                temp.append(float(j))
+            X.append(temp)
+            temp = []
+            if item[-1] == 'Iris-setosa':
+                y.append(0)
+            elif item[-1] == 'Iris-versicolor':
+                y.append(1)
+            elif item[-1] == 'Iris-virginica':
+                y.append(2)
 
-print(X)
-print(y)
-# print(labels_dict)
-print(class_labels)
-# print(class_labels.index('Iris-setosa'))
+        i = i + 1
+
+    return [X, y, class_labels]
+
+
+X, y, class_labels = load_iris_dataset('iris.data')
+
+print('X =', X)
+print('y =', y)
+print('class labels =', class_labels)
 
