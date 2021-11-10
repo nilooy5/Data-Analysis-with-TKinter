d3592aabed4c2761ed2e6c6b5861b961ee96a37b
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-11-11 03:35:22 +0600

    q4 zero division error handled

--- question_4.py
+++ question_4.py
@@ -18,16 +18,24 @@ while True:
     d = float(input("Enter the specific depth (in meters):"))
     # Vt = 0.25
     # d = 1.8
-    is_SI = input("Enter the specific depth (in meters):")
+    is_SI = input("Do you wish the results using the International System of Units?:")
 
     if is_SI == "Yes" or is_SI == "yes":
-        result = my_formula(Vt, d)
+        try:
+            result = my_formula(Vt, d)
+        except ZeroDivisionError:
+            print("Error: Zero Division Error")
+            continue
         result = round(result, 3)
         result = str(result) + " m/min"
     else:
         Vt = meterCube_to_feetCube(Vt)
         d = meter_to_feet(d)
-        result = my_formula(Vt, d)
+        try:
+            result = my_formula(Vt, d)
+        except ZeroDivisionError:
+            print("Error: Zero Division Error")
+            continue
         result = round(result, 3)
         result = str(result) + " feet/min"
 
