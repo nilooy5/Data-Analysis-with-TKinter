aa52bff93f78d7b26bbfa983f8b7524171410332
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-11-10 12:02:10 +0600

    question_5.py solved

--- question_5.py
+++ question_5.py
@@ -1,26 +1,32 @@
 import matplotlib.pyplot as plt
 from sklearn import datasets
 
-a = 2
-b = 3
+while True:
+    print("input 2 numbers from between 0 to 3.")
+    a = int(input("a = "))
+    b = int(input("b = "))
 
-iris = datasets.load_iris()  # returns np-array (150 rows and 4 columns)
+    if a < 0 or a > 3 or b < 0 or b > 3:
+        print("input error.")
+        continue
 
-print(iris['feature_names'])
+    iris = datasets.load_iris()  # returns np-array (150 rows and 4 columns)
 
-# Each row is a 4D sample
-# Column: Sepal Length, Sepal Width, Petal Length and Petal Width.
-X = iris.data[:, [a, b]]  # take the first two columns.
-y = iris.target
-# print("X =", X)
-# print("X-0 =", X[:, 0])
-# print("X-1 =", X[:, 1])
-# Target names: Setosa, Versicolour, and Virginica
-labels = iris.target_names
-# Plot the data
-plt.scatter(X[:, 0], X[:, 1], label='iris', c='r', marker='o', s=30)
-plt.xlabel(iris['feature_names'][a])
-plt.ylabel(iris['feature_names'][b])
-plt.title('Iris dataset')
-plt.legend()
-plt.show()
+    # print(iris['feature_names'])
+
+    # Each row is a 4D sample
+    # Column: Sepal Length, Sepal Width, Petal Length and Petal Width.
+    X = iris.data[:, [a, b]]  # take the first two columns.
+    y = iris.target
+    # print("X =", X)
+    # print("X-0 =", X[:, 0])
+    # print("X-1 =", X[:, 1])
+    # Target names: Setosa, Versicolour, and Virginica
+    labels = iris.target_names
+    # Plot the data
+    plt.scatter(X[:, 0], X[:, 1], label='iris', c='r', marker='o', s=30)
+    plt.xlabel(iris['feature_names'][a])
+    plt.ylabel(iris['feature_names'][b])
+    plt.title('Iris dataset')
+    plt.legend()
+    plt.show()
